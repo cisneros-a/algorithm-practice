2f0ff0e9c1c6c841ebdb011e3c12fe3fada91615
github username <github email address>
2020-07-03 11:41:03 -0500

    added candles and minimax problem

--- algorithms/hackerrank/hacker.js
+++ algorithms/hackerrank/hacker.js
@@ -15,4 +15,33 @@ function staircase(n) {
   }
 }
 
-staircase(6);
+// staircase(6);
+
+//Mini-Max Sum
+
+function miniMaxSum(arr) {
+  let total = arr.reduce((acc, val) => {
+    return acc + val;
+  });
+  console.log(total - arr[arr.length - 1], total - arr[0]);
+}
+
+// miniMaxSum([1, 2, 3, 4, 5]);
+
+// Birthday Cake Candles
+function birthdayCakeCandles(ar) {
+  let largestNum = 0;
+  let largestNumCount = 1;
+  for (let i = 0; i < ar.length; i++) {
+    if (ar[i] === largestNum) {
+      largestNumCount += 1;
+    }
+    if (ar[i] > largestNum) {
+      largestNum = ar[i];
+      largestNumCount = 1;
+    }
+  }
+  console.log(largestNumCount);
+}
+
+birthdayCakeCandles([3, 2, 1, 3]);
