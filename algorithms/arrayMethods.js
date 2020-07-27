f006992d2d3c3e6d102a573a3cbeb142fc66ed34
github username <github email address>
2020-07-26 23:12:05 -0500

    array methods cont for blog

--- algorithms/arrayMethods.js
+++ algorithms/arrayMethods.js
@@ -11,3 +11,16 @@ nums.includes(7); // false
 
 nums.every((num) => num < 3); // false
 nums.some((num) => num < 3); // true
+
+const numsToo = [6, 7, 8, 9, 10];
+
+nums.concat(numsToo); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+nums.indexOf(3); // 2
+
+nums.reverse(); // [5,4,3,2,1]
+
+nums.join(); // "1,2,3,4,5"
+nums.join(""); // "12345"
+nums.join(" "); // "1 2 3 4 5"
+nums.join("-"); // "1-2-3-4-5"
